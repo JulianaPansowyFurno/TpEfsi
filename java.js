7575f0964faad20780f3d17fd9ec12a01150b447
JULIANA PANSOWY FURNO <47350201@ort>
2023-05-10 11:15:37 -0300

    inicio

---
+++ java.js
@@ -0,0 +1,27 @@
+fetch(" http://www.omdbapi.com/?i=tt3896198&apikey=da6c8f77&s=titanic")
+    .then(res => res.json())
+    .then(res => {
+        console.log("obtuve respuesta")
+        valores = document.getElementById("valores");
+        res.Search.forEach(actual => {
+                peli = document.createElement("li")
+                peli.innerHTML = `Nombre: ${actual.Title} - Año: ${actual.Year}`
+                valores.appendChild(peli)    
+        })
+    })
+    .catch(err => console.error("error", err))
+console.log("Fin consulta - fetch")
+
+fetch(" https://omdbapi.com/?apikey=da6c8f77&t=elite&Season=1&Episode=1")
+    .then(res => res.json())
+    .then(res => {
+        console.log("obtuve respuesta")
+        valores = document.getElementById("valores");
+        res.Search.forEach(actual => {
+            peli = document.createElement("li")
+            peli.innerHTML = `Nombre: ${actual.Title} - Episode: ${actual.Episode}`
+            valores.appendChild(peli)    
+        })
+    })
+    .catch(err => console.error("error", err))
+console.log("Fin consulta - fetch")
